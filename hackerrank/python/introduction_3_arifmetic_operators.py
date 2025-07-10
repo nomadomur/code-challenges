7f7123dfb3afb6b3bff92196ab589a28ed8bdbef
nomadomur <umurzokwbs@gmail.com>
2025-07-10 19:30:54 +0900

    arifmetic and loop

--- hackerrank/python/introduction_3_arifmetic_operators.py
+++ hackerrank/python/introduction_3_arifmetic_operators.py
@@ -4,9 +4,25 @@
 # The second line contains the difference of the two numbers (first - second).
 # The third line contains the product of the two numbers.
 
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
+# ----- 1 ----------
+def calculate(a, b):
     print(a + b)
     print(a - b)
     print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    calculate(a, b)
+# ----- 2 ----------
+
+# if __name__ == '__main__':
+#     a, b = int(input()), int(input())
+#     for rlt in (a + b, a - b, a * b):
+#         print(rlt)
+        
+# ----- 3 ----------
+
+# if __name__ == '__main__':
+#     a, b = map(int, [input(), input()])
+#     print(a + b, a - b, a * b, sep='\n')
