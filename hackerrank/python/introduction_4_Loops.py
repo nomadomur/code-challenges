57515b2dc7e46fc0fe9179995000d8787f3d6d30
nomadomur <umurzokwbs@gmail.com>
2025-07-10 19:37:14 +0900

    loop

--- hackerrank/python/introduction_4_Loops.py
+++ hackerrank/python/introduction_4_Loops.py
@@ -1,5 +1,7 @@
 # The provided code stub reads an integer, n, from STDIN. For all non-negative integers i < n, print i^2.
 
+# ----------- 1 ----------
+
 def sqr_n(n):
     for i in range(n):
         print(i * i)
@@ -7,3 +9,12 @@ def sqr_n(n):
 if __name__ == '__main__':
     n = int(input())
     sqr_n(n)
+
+# ----------- 2 ----------
+
+# def sqr_n(n):
+#     list(map(lambda i: print(i * i), range(n)))
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     sqr_n(n)
