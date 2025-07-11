6e799b84c3b499916bc9c61538434befaa51b7c5
nomadomur <umurzokwbs@gmail.com>
2025-07-11 23:13:31 +0900

    func and diision

--- hackerrank/python/introduction_5_WriteAFunction.py
+++ hackerrank/python/introduction_5_WriteAFunction.py
@@ -7,13 +7,18 @@
 #       The year is also evenly divisible by 400. Then it is a leap year.
 # This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.
 
-def is_leapyear(year):
+
+def is_leap(year):
     leap = False
-    
-    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-        leap = True
-        
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap = True
     return leap
+
+    
 if __name__ == '__main__':
     year = int(input())
-    print(is_leapyear(year))
+    print(is_leap(year))
